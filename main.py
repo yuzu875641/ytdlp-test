b3eff2467049952ee54c4241cf45f5868fbc28f0
FoxeiZ <foxeiz2004@gmail.com>
2024-05-07 08:27:41 +0700

    init

---
+++ main.py
@@ -0,0 +1,16 @@
+from yt_dlp import YoutubeDL
+from fastapi import FastAPI
+
+app = FastAPI()
+ytdl = YoutubeDL()
+
+
+@app.get("/")
+async def index():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app)
