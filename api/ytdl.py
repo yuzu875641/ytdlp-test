2f20e98a85df85fc7344491e4df75119559952f2
FoxeiZ <foxeiz2004@gmail.com>
2024-05-08 19:45:18 +0700

    update

--- api/ytdl.py
+++ api/ytdl.py
@@ -1,4 +1,5 @@
 from uuid import uuid4
+from typing import Iterable
 
 from flask import Flask, jsonify, request
 from yt_dlp import YoutubeDL, DownloadError
@@ -20,10 +21,32 @@ ytdlopts = {
     "quiet": True,
     "extract_flat": "in_playlist",
     "no_warnings": True,
-    "default_search": "ytsearch5",
     "source_address": "0.0.0.0",
 }
-ytdl = YoutubeDL(ytdlopts)
+
+
+def get_extractor(
+    config: dict | None = None, provider: str = "youtube", search_amount: int = 5
+) -> YoutubeDL:
+    if config is None:
+        config = ytdlopts
+    else:
+        config = {**ytdlopts, **config}
+
+    match provider:
+        case "soundcloud":
+            config["default_search"] = f"scsearch{search_amount}"
+        case "ytmusic":
+            config.update(
+                {
+                    "default_search": "https://music.youtube.com/search?q=",
+                    "playlist_items": f"1-{search_amount}",
+                }
+            )
+        case _:
+            config["default_search"] = f"ytsearch{search_amount}"
+
+    return YoutubeDL(config)
 
 
 def str_to_bool(value: str | bool | None) -> bool:
@@ -65,32 +88,18 @@ def login():
     return "", 401
 
 
-def require_auth(func):
-    def wrapper(*args, **kwargs):
-        if not login_users:
-            return func(*args, **kwargs)
+def extract_info(
+    extractor: YoutubeDL, url=None, video_id=None, process: bool | str | None = True
+):
+    if not url and not video_id:
+        return jsonify({"error": "No url or video_id provided"}), 400
 
-        auth_token = request.headers.get("Authorization")
-        if not auth_token:
-            auth_token = request.args.get("token") or request.form.get("token")
-
-        if auth_token and auth_token in login_users:
-            return func(*args, **kwargs)
-        else:
-            return jsonify({"error": "Invalid token"}), 401
-
-    wrapper.__name__ = func.__name__
-
-    return wrapper
-
-
-def extract_info(url=None, video_id=None, process: bool | str | None = True):
     process = str_to_bool(process)
     if video_id:
         url = f"https://www.youtube.com/watch?v={video_id}"
 
     try:
-        info = ytdl.extract_info(url, download=False, process=process)
+        info = extractor.extract_info(url, download=False, process=process)
     except DownloadError as e:
         return jsonify({"error": str(e)}), 400
     except Exception:
@@ -99,24 +108,72 @@ def extract_info(url=None, video_id=None, process: bool | str | None = True):
     if info is None:
         return jsonify({"error": "Failed to extract info"}), 500
 
+    info["is_search"] = info.get("extractor", "").find("search") != -1
     return jsonify(info)
 
 
-@app.route(PREFIX + "/video/<video_id>")
-@require_auth
-def ytdl_api(video_id):
-    process = request.args.get("process")
-    return extract_info(video_id=video_id, process=process)
+def require_argument(arguments: Iterable[str]):
+    """Decorator that checks if the given argument name is present in the request JSON,
+    and raises a 400 error if not.
+    """
+
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            json_data = request.get_json(force=True, silent=True, cache=True)
+            if not json_data:
+                return "Malformed JSON", 400
+
+            for argument in arguments:
+                if argument not in json_data:
+                    return "Missing argument", 400
+                kwargs[argument] = json_data[argument]
+
+            return func(*args, **kwargs)
+
+        wrapper.__name__ = func.__name__
+        return wrapper
+
+    return decorator
 
 
-@app.route(PREFIX + "/playlist/<string:playlist_id>")
-@require_auth
-def ytdl_api_playlist(playlist_id: str):
+def check_arguments(arguments: Iterable[str]):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            json_data = request.get_json(force=True, silent=True, cache=True)
+            if not json_data:
+                return "Malformed JSON", 400
+
+            for argument in arguments:
+                if argument not in json_data:
+                    continue
+
+                kwargs[argument] = json_data[argument]
+            return func(*args, **kwargs)
+
+        wrapper.__name__ = func.__name__
+        return wrapper
+
+    return decorator
+
+
+@app.post(PREFIX + "/query")
+@require_argument(["query"])
+@check_arguments(["process", "provider", "search_amount"])
+def search(
+    query: str, process: bool = True, provider: str = "youtube", search_amount: int = 5
+):
     return extract_info(
-        url=f"https://www.youtube.com/playlist?list={playlist_id}",
-        process=request.args.get("process"),
+        get_extractor(provider=provider, search_amount=search_amount),
+        url=query,
+        process=process,
     )
 
 
+@app.post(PREFIX + "/get_stream")
+@require_argument(["url"])
+def get_stream(url: str):
+    return extract_info(get_extractor(), url=url, process=True)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
