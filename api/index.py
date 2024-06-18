1d8aab21002fc9bdf9a6ee53a8e437cad95c815d
FoxeiZ <foxeiz2004@gmail.com>
2024-06-18 08:42:44 +0700

    new url rule

--- api/index.py
+++ api/index.py
@@ -1,8 +1,21 @@
-from flask import Flask
+import os
 
-app = Flask(__name__)
+from flask import Flask, render_template
+
+PREFIX = "/"
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+
+app = Flask(
+    __name__,
+    template_folder=os.path.join(BASEDIR, *[os.path.pardir, "templates"]),
+    static_folder=os.path.join(BASEDIR, *[os.path.pardir, "static"]),
+)
 
 
-@app.route("/api")
-def home():
-    return "Hello, World!"
+@app.route(PREFIX)
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
