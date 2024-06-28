498691374cd92e2f9db47747f6ba75401b69a1f7
FoxeiZ <foxeiz2004@gmail.com>
2024-06-28 16:09:29 +0700

    maybe this is less spaghetti? idk

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -32,44 +32,64 @@ function isValidHttpUrl(string) {
   return url.protocol === "http:" || url.protocol === "https:";
 }
 
-function resetDownloadText(setOriginal = true) {
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function resetDownloadText(setOriginal = true) {
   const downloadText = document.getElementById("download-text")
   downloadText.classList.remove("red")
+  downloadText.classList.remove("no-opacity")
   if (setOriginal) {
     downloadText.innerHTML = "download"
   }
 }
 
-function animateDownloadText(afterText = "download", animation = true) {
-  const downloadText = document.getElementById("download-text")
-  if (animation) {
-    downloadText.classList.add("no-opacity")
+async function animateDownloadText(afterText = "download", options = { animation: true, isError: false }) {
+  const downloadText = document.getElementById("download-text");
+  if (options.animation) {
+    downloadText.classList.add("no-opacity");
+    await sleep(200);
   }
-  setTimeout(() => {
-    resetDownloadText(false)
-    downloadText.innerHTML = afterText
-    downloadText.classList.remove("no-opacity")
-  }, 200)
+  resetDownloadText(false);
+
+  if (options.isError) {
+    downloadText.classList.add("red");
+  }
+  downloadText.innerHTML = afterText;
+  downloadText.classList.remove("no-opacity");
 }
 
-function animateErrorText(afterText) {
-  const downloadText = document.getElementById("download-text")
-  resetDownloadText(false)
-  downloadText.classList.add("no-opacity")
-  setTimeout(() => {
-    downloadText.innerHTML = afterText
-    downloadText.classList.add("red")
-    downloadText.classList.remove("no-opacity")
-  }, 200)
+async function animateErrorText(afterText) {
+  await animateDownloadText(afterText, { isError: true });
 }
 
-function submit(url) {
+function submitWrapper(url) {
+  const downloadBtn = document.getElementById("download-button");
+  if (downloadBtn.classList.contains("disabled")) {
+    return;
+  }
+  downloadBtn.classList.add("disabled");
+  submit(url)
+    .then(async (errorMsg) => {
+      if (errorMsg != undefined) {
+        animateErrorText(errorMsg);
+      }
+    })
+    .finally(() => {
+      sleep(2000).then(() => {
+        animateDownloadText("download");
+        downloadBtn.classList.remove("disabled");
+      });
+    });
+}
+
+async function submit(url) {
   if (!isValidHttpUrl(url)) {
     return
   }
   animateDownloadText("...")
-
-  fetch("/api/ytdl/check", {
+  const response = await fetch("/api/ytdl/check", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -79,24 +99,25 @@ function submit(url) {
       type: document.getElementsByClassName("av-wrapper")[0].getAttribute("data-value"),
       has_ffmpeg: window.WP_ffmpeg !== undefined && window.WP_ffmpeg.loaded
     }),
-  }).then((response) => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  })
+  });
+
+  if (!response.ok) {
+    const errorJson = await response.json();
+    window.WP_notifier.warning(errorJson.error);
+    return;
+  }
+
+  animateDownloadText("downloading...");
+  return await response.json()
     .then(async (data) => {
-      animateDownloadText("downloading...");
       if (data.needs_ffmpeg == true) {
         if (!window.WP_ffmpeg.loaded) {
-          animateErrorText("ffmpeg not loaded");
-          return;
+          return "ffmpeg not loaded";
         }
-
         const params = {
           filename: data.title,
         };
-        debugger;
+
         for (const format of data.requested_formats) {
           const fileData = format.is_part ? await partsDownload(format) : await window.WP_fetchFile(`/api/ytdl/download?video_id=${format.video_id}`);
           params[`${format.type}Data`] = fileData;
@@ -104,18 +125,16 @@ function submit(url) {
           params[`${format.type}Title`] = format.format_id;
         }
         await ffmpegDownload(params);
-        animateDownloadText("download");
         return;
       }
 
       if (data.is_part == true) {
-        return partsDownload(data)
-          .then((data) => {
-            saveAs(data, data.name);
-            animateDownloadText("download");
+        return await partsDownload(data)
+          .then((blob) => {
+            saveAs(blob, data.title + "." + data.ext);
           })
           .catch(() => {
-            animateErrorText("invalid url");
+            return "I/O error";
           });
       }
       download(data);
@@ -162,7 +181,7 @@ async function parts(bufferList, url, video_id, filesize_approx, range_start) {
   const contentLength = getResp.headers.get("Content-Length")
   const new_range_start = Number(range_start) + Number(contentLength)
 
-  animateDownloadText(`downloading... ${humanFileSize(new_range_start)}/${humanFileSize(filesize_approx)}`, false)
+  animateDownloadText(`downloading... ${humanFileSize(new_range_start)}/${humanFileSize(filesize_approx)}`, { animation: false })
 
   arrayBuffer = await getResp.arrayBuffer()
   bufferList.push(arrayBuffer)
@@ -170,7 +189,6 @@ async function parts(bufferList, url, video_id, filesize_approx, range_start) {
 }
 
 async function ffmpegDownload({ filename, videoData, videoTitle, videoExt, audioData, audioTitle, audioExt }) {
-  debugger;
   const videoName = videoTitle + "." + videoExt
   const audioName = audioTitle + "." + audioExt
   const outputName = filename + "." + videoExt
@@ -241,9 +259,6 @@ function download(data) {
   a.href = `/api/ytdl/download?video_id=${data.video_id}`;
   a.download = data.title + "." + data.ext;
   a.click();
-  setTimeout(() => {
-    animateDownloadText()
-  }, 5000)
 }
 
 function checkInput(input) {
