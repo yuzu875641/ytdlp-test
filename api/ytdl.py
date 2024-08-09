9294d68b80833626430bd4e902f479b57b4f1fd6
FoxeiZ <foxeiz2004@gmail.com>
2024-08-09 14:42:56 +0700

    add classlist support for macro

--- api/ytdl.py
+++ api/ytdl.py
@@ -1,5 +1,5 @@
 import os
-from typing import Any, Iterable
+from typing import Any, Iterable, MutableSet
 from base64 import urlsafe_b64encode, urlsafe_b64decode
 
 from flask import (
@@ -37,6 +37,55 @@ ytdlopts = {
     "no_warnings": True,
     "source_address": "0.0.0.0",
 }
+
+
+@app.template_global("classlist")
+class ClassList(MutableSet):
+    """Data structure for holding, and ultimately returning as a single string,
+    a set of identifiers that should be managed like CSS classes.
+    """
+
+    def __init__(self, arg=None, default: Iterable[str] | None = None):
+        """Constructor.
+        :param arg: A single class name or an iterable thereof.
+        """
+        if isinstance(arg, str):
+            classes = arg.split()
+        elif isinstance(arg, Iterable):
+            classes = arg
+        elif arg is not None:
+            raise TypeError("expected a string or string iterable, got %r" % type(arg))
+
+        self.classes = set(filter(None, classes))
+
+        if default is not None:
+            self.add(*default)
+
+    def __contains__(self, class_):
+        return class_ in self.classes
+
+    def __iter__(self):
+        return iter(self.classes)
+
+    def __len__(self):
+        return len(self.classes)
+
+    def add(self, *classes):
+        for class_ in classes:
+            self.classes.add(class_)
+        return ""
+
+    def discard(self, *classes):
+        for class_ in classes:
+            self.classes.discard(class_)
+
+        return ""
+
+    def __str__(self):
+        return " ".join(sorted(self.classes))
+
+    def __html__(self):
+        return 'class="%s"' % self if self else ""
 
 
 def get_extractor(
@@ -394,4 +443,4 @@ def download(
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
