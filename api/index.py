499b45faa61cace99287201774e52e792d7c13db
FoxeiZ <foxeiz2004@gmail.com>
2024-08-11 09:27:22 +0700

    apparently vercel cant import util class

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 import os
-
+from typing import Iterable, MutableSet
 from flask import Flask, render_template
-from utils import ClassList
+
 
 PREFIX = "/"
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
@@ -11,7 +11,54 @@ app = Flask(
     template_folder=os.path.join(BASEDIR, *[os.path.pardir, "templates"]),
     static_folder=os.path.join(BASEDIR, *[os.path.pardir, "static"]),
 )
-app.add_template_global(ClassList, name="classlist")
+
+
+@app.template_global("classlist")
+class ClassList(MutableSet):
+    """Data structure for holding, and ultimately returning as a single string,
+    a set of identifiers that should be managed like CSS classes.
+    """
+
+    def __init__(self, arg: str | Iterable | None = None, *args: str):
+        """Constructor.
+        :param arg: A single class name or an iterable thereof.
+        """
+        if isinstance(arg, str):
+            classes = arg.split()
+        elif isinstance(arg, Iterable):
+            classes = arg
+        elif arg is not None:
+            raise TypeError("expected a string or string iterable, got %r" % type(arg))
+
+        self.classes = set(filter(None, classes))
+        if args:
+            self.classes.update(args)
+
+    def __contains__(self, class_):
+        return class_ in self.classes
+
+    def __iter__(self):
+        return iter(self.classes)
+
+    def __len__(self):
+        return len(self.classes)
+
+    def add(self, *classes):
+        for class_ in classes:
+            self.classes.add(class_)
+        return ""
+
+    def discard(self, *classes):
+        for class_ in classes:
+            self.classes.discard(class_)
+
+        return ""
+
+    def __str__(self):
+        return " ".join(sorted(self.classes))
+
+    def __html__(self):
+        return 'class="%s"' % self if self else ""
 
 
 @app.route(PREFIX)
