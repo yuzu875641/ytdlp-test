796939af85954d903576d81efd6b08752b19dbcc
FoxeiZ <57582539+FoxeiZ@users.noreply.github.com>
2025-04-28 13:56:33 +0700

    more offset

--- api/gelbooru.py
+++ api/gelbooru.py
@@ -109,7 +109,7 @@ def is_fit_aspect_ratio(
     if not width or not height:
         return False
 
-    return abs((width / height) - aspect_ratio) < 0.1
+    return abs((width / height) - aspect_ratio) < 0.15
 
 
 def select_image(data: dict, aspect_ratio: float | None = None) -> str:
